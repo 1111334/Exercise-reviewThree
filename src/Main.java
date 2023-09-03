74e95bcfec1d75e9fab935c83f87be872aae8b52
Mike <109431533+1111334@users.noreply.github.com>
2023-09-03 16:23:47 +0200

    review

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student[] students = new Student[3];
+        students[0] = new Student(3636, "Marco Rossi", 25.0, 30.0);
+        students[1] = new Student(4775, "Luca Rossi", 27.0, 29.5);
+        students[2] = new Student(6859, "Elena Rossi", 30.0, 32.0);
+
+        System.out.println("Excellent student: ");
+        Student.printExcellentStudents(students);
+
+        for (Student student : students) {
+            //System.out.println("Best Grade: " + student.getBestGrade());
+            System.out.println("Full name: " + student.getFullName() + "\n" + "full identifier: " + student.getIdentifier());
+        }
+
+
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    private long identifier;
+
+    private String fullName;
+    private double worstGrade;
+    private static double bestGrade;
+
+    public Student(long identifier, String fullName, double worstGrade, double bestGrade) {
+        this.identifier = identifier;
+        this.fullName = fullName;
+        this.worstGrade = worstGrade;
+        Student.bestGrade = bestGrade;
+    }
+    public long getIdentifier() {
+        return identifier;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public double getBestGrade() {
+        return bestGrade;
+    }
+
+    public static void printExcellentStudents(Student[] arrayStudent) {
+        for (Student student : arrayStudent) {
+            if (student.getBestGrade() > 28.0) {
+                System.out.println(student.getFullName());
+            }
+        }
+
+    }
+}
